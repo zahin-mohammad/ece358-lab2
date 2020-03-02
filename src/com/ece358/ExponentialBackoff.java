326811bba22273ad9906bda5eda73b000c9d4f84
Zahin Mohammad <zahin.dev@gmail.com>
2020-03-02 10:03:29 -0500

    implement exponential backoff

--- src/com/ece358/ExponentialBackoff.java
+++ src/com/ece358/ExponentialBackoff.java
@@ -1,4 +1,32 @@
 package com.ece358;
 
+import java.util.Random;
+
 public class ExponentialBackoff {
+    private Random r;
+    private int counter = 0;
+    private int counterSaturation = 0;
+    private int bitTime = 0;
+
+    ExponentialBackoff(int linkCapacity){
+        this(0, 10, 512, linkCapacity);
+    }
+    ExponentialBackoff(int counter, int counterSaturation, int bitTimeSize, int linkCapacity){
+        this.r = new Random();
+        this.bitTime = bitTimeSize/linkCapacity;
+        this.counter = counter;
+        this.counterSaturation = counterSaturation;
+    }
+
+    public double getWaitTime(){
+        counter++;
+        if (counter > counterSaturation){
+            // TODO: Throw error?
+            System.out.println("Counter greater than saturation");
+        }
+        return r.nextInt((int) Math.pow(2,counter))*bitTime;
+    }
+    public void resetCounter(){
+        counter = 0;
+    }
 }
